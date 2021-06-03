3f76de12114421301c321069a888ba40158f7ae2
tbdpusheen <baconbalony@gmail.com>
2021-06-02 19:30:51 -0700

    Finished question 1

--- Algorithms.java
+++ Algorithms.java
@@ -2,24 +2,35 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-        int[] testArray = {1, 2, 3, 4,3,2,4,1,2,5,6,2,4,2,1};
-        int testSum = 6;
+        int[] testArray = {1,5,6,2,3,72,2,5,3,1,123,12,11,10};
+        int testSum = 13;
         findPairs(testArray, testSum);
     }
 
     private static void findPairs(int[] testArray, int targetSum) {
-        ArrayList<Integer> numbersArray = new ArrayList<>();
+        // Creates a copy of the input array and sorts it, simplifying mulitple aspects of the algorithm
+        int[] sortedArray = testArray;
+        Arrays.sort(sortedArray);
         ArrayList<String> pairsArray = new ArrayList<>();
 
-        for (int num1 : testArray) {
-            for (Integer num2 : numbersArray) {
-                String pair = "(" + Math.min(num1, num2) + ", " + Math.max(num1, num2) + ")";
-                if (num1 + num2 == targetSum && !pairsArray.contains(pair)) {
-                    pairsArray.add(pair);
-                    System.out.println(pair);
+        int currentIndex = 0;
+        for (int num1 : sortedArray) {
+            // Goes through the array looking for pairs. If the current number is higher than the target,
+            // break, as the array is sorted and all subsequent numbers will also be higher
+            if (num1 <= targetSum) {
+                // Looks for all number higher than the current one, as all previous numbers will have
+                // been tried and paired
+                for (int loopingIndex = ++currentIndex; loopingIndex < sortedArray.length; loopingIndex++) {
+                    // Creates a pair which always starts with the smaller number, as the array is sorted
+                    String pair = "(" + num1 + ", " + sortedArray[loopingIndex] + ")";
+                    if (num1 + sortedArray[loopingIndex] == targetSum && !pairsArray.contains(pair)) {
+                        pairsArray.add(pair);
+                        System.out.println(pair);
+                    }
                 }
+            } else {
+                break;
             }
-            numbersArray.add(num1);
         }
     }
 }
