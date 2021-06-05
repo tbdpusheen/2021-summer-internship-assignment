7a4f3f7828eb3bd3b1be87309884f90491e6fb2c
tbdpusheen <baconbalony@gmail.com>
2021-06-04 21:35:46 -0700

    Cleaned up android project

--- summerinternshipapplication/app/src/main/java/com/example/summerinternshipapplication/MainActivity.java
+++ summerinternshipapplication/app/src/main/java/com/example/summerinternshipapplication/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.summerinternshipapplication;
 
-import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
