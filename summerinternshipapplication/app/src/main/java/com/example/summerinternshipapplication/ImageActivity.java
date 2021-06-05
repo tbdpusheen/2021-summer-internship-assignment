15c4b1d0ec5c7a4db83cca206e96a7ae2dfcc618
tbdpusheen <baconbalony@gmail.com>
2021-06-04 21:30:51 -0700

    Finished challenge 2

--- summerinternshipapplication/app/src/main/java/com/example/summerinternshipapplication/ImageActivity.java
+++ summerinternshipapplication/app/src/main/java/com/example/summerinternshipapplication/ImageActivity.java
@@ -3,6 +3,7 @@ package com.example.summerinternshipapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class ImageActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class ImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        // Sets the ImageView to the image passed in to the activity
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(getIntent().getIntExtra("Image", 0));
     }
 }
--- summerinternshipapplication/app/src/main/java/com/example/summerinternshipapplication/MainActivity.java
+++ summerinternshipapplication/app/src/main/java/com/example/summerinternshipapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.summerinternshipapplication;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
@@ -14,67 +16,71 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ArrayList<String> images = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Field[] drawablesFields = R.drawable.class.getDeclaredFields();
-        ArrayList<Drawable> drawables = new ArrayList<>();
-        for (Field field : drawablesFields) {
-            try {
-                drawables.add(ContextCompat.getDrawable(this, field.getInt(null)));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
+        ArrayList<Integer> images = new ArrayList<>();
+        // Loops through all drawables with the prefix "flower_" and adds their id to an ArrayList
+        for (int i = 1; getResources().getIdentifier("flower_" + i,
+                "drawable", getPackageName()) != 0; i++) {
+            images.add(getResources().getIdentifier("flower_" + i, "drawable", getPackageName()));
         }
 
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        recyclerView.setAdapter(new Adapter(this, images));
     }
 
-    //  ViewHolder
+    // ViewHolder for the RecyclerView
     private static class ViewHolder extends RecyclerView.ViewHolder {
 
         private final ImageView imageView;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.imageView);
         }
     }
 
-    // Adapter
+    // Adapter for the RecyclerView
     private static class Adapter extends RecyclerView.Adapter<ViewHolder> {
 
         private final Context context;
-        Adapter(Context context) {
+        private final ArrayList<Integer> images;
+
+        Adapter(Context context, ArrayList<Integer> images) {
             this.context = context;
+            this.images = images;
         }
 
         @NonNull
         @Override
         public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             LayoutInflater inflater = LayoutInflater.from(context);
-            return new ViewHolder(inflater.inflate(0, parent, false));
+            return new ViewHolder(inflater.inflate(R.layout.item_image, parent, false));
         }
 
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+            // Sets the ImageView to the resource id of the current image
+            holder.imageView.setImageResource(images.get(position));
+            // When an image is selected, go to the image activity and pass in the selected image
             holder.itemView.setOnClickListener(v -> {
                 Intent intent = new Intent(context, ImageActivity.class);
+                intent.putExtra("Image", (int) images.get(position));
                 context.startActivity(intent);
             });
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return images.size();
         }
     }
-
 }
