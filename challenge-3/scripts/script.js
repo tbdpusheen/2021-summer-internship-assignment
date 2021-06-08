818d979cdf78e43e338d73486916e5e80659578a
tbdpusheen <baconbalony@gmail.com>
2021-06-07 22:19:08 -0700

    Finished overlay

--- challenge-3/scripts/script.js
+++ challenge-3/scripts/script.js
@@ -1,24 +1,32 @@
-// Searches for all elements with the 'image' class
-const images = document.querySelectorAll('.image');
-images.forEach(image => { // Adds a click event listener to every element, which opens the overlay
-  image.addEventListener('click', function(){openOverlay(image.id)})
-});
+window.onload = () => {
+  // Searches for all elements with the 'image' class
+  const images = document.querySelectorAll('.image');
+  images.forEach(image => { // Adds a click event listener to every element which opens the overlay
+    image.addEventListener('click', () => { openOverlay(image.id); });
+  });
 
-// Opens the overlay page depending on which image was clicked
-function openOverlay(imageId) {
-  switch(imageId) {
-    case 'top-left-image':
-      document.location.href = 'html/top-left-image.html';
-      break;
-    case 'top-right-image':
-      document.location.href = 'html/top-right-image.html';
-      break;
-    case 'bottom-left-image':
-      document.location.href = 'html/bottom-left-image.html';
-      break;
-    case 'bottom-right':
-      document.location.href = 'html/bottom-right-image.html';
-      break;
-    default:
+  // Opens the overlay page depending on which image was clicked
+  function openOverlay(imageId) {
+    switch(imageId) {
+      case 'top-left-image':
+        document.location.href = 'html/top-left-image.html';
+        break;
+      case 'top-right-image':
+        document.location.href = 'html/top-right-image.html';
+        break;
+      case 'bottom-left-image':
+        document.location.href = 'html/bottom-left-image.html';
+        break;
+      case 'bottom-right':
+        document.location.href = 'html/bottom-right-image.html';
+        break;
+      default:
+    }
   }
-}
+
+  // Adds a click event listener to the close button which closes the overlay
+  const closeButton = document.querySelector('#close');
+  if (closeButton != null) {
+    closeButton.addEventListener('click', () => { document.location.href = '../index.html'; });
+  }
+};
