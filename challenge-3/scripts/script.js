09abf6053fb0b2e80ca0ec2590db9aa7c2bfb5be
tbdpusheen <baconbalony@gmail.com>
2021-06-08 22:16:04 -0700

    Added hover effect

--- challenge-3/scripts/script.js
+++ challenge-3/scripts/script.js
@@ -2,27 +2,25 @@ window.onload = () => {
   // Searches for all elements with the 'image' class
   const images = document.querySelectorAll('.image');
   images.forEach(image => { // Adds a click event listener to every element which opens the overlay
-    image.addEventListener('click', () => { openOverlay(image.id); });
+    image.addEventListener('click', () => {
+      // Opens the overlay page depending on which image was clicked
+      switch(image.id) {
+        case 'top-left-image':
+          document.location.href = 'html/top-left-image.html';
+          break;
+        case 'top-right-image':
+          document.location.href = 'html/top-right-image.html';
+          break;
+        case 'bottom-left-image':
+          document.location.href = 'html/bottom-left-image.html';
+          break;
+        case 'bottom-right':
+          document.location.href = 'html/bottom-right-image.html';
+          break;
+        default:
+      }
+    });
   });
-
-  // Opens the overlay page depending on which image was clicked
-  function openOverlay(imageId) {
-    switch(imageId) {
-      case 'top-left-image':
-        document.location.href = 'html/top-left-image.html';
-        break;
-      case 'top-right-image':
-        document.location.href = 'html/top-right-image.html';
-        break;
-      case 'bottom-left-image':
-        document.location.href = 'html/bottom-left-image.html';
-        break;
-      case 'bottom-right':
-        document.location.href = 'html/bottom-right-image.html';
-        break;
-      default:
-    }
-  }
 
   // Adds a click event listener to the close button which closes the overlay
   const closeButton = document.querySelector('#close');
