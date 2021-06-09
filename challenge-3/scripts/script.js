bf19e912b9fa5103b5127a469d1fcb60288f8933
tbdpusheen <baconbalony@gmail.com>
2021-06-08 23:07:50 -0700

    Disabled overlay scrolling

--- challenge-3/scripts/script.js
+++ challenge-3/scripts/script.js
@@ -1,4 +1,31 @@
 window.onload = () => {
+  linkImages();
+  disableOverlayScrolling();
+  setupClose();
+};
+
+// Disable scrolling on overlay pages.
+function disableOverlayScrolling() {
+  if (document.querySelector('#overlay') != null) {
+    setInterval(() => {
+      const html = document.querySelector('html');
+      // Enable the scrollbar even without scrolling so as to preserve the width of the page.
+      // Only enable the scrollbar if it was supposed to be there already
+      if (html.offsetHeight > window.innerHeight) {
+        html.style.position = "fixed";
+        html.style.overflowY = "scroll";
+        html.style.width = "100%";
+      } else {
+        html.style.position = "";
+        html.style.overflowY = "";
+        html.style.width = "";
+      }
+    })
+  }
+}
+
+// Links the images to their respective overlays
+function linkImages() {
   // Searches for all elements with the 'image' class
   const images = document.querySelectorAll('.image');
   images.forEach(image => { // Adds a click event listener to every element which opens the overlay
@@ -21,10 +48,12 @@ window.onload = () => {
       }
     });
   });
+}
 
-  // Adds a click event listener to the close button which closes the overlay
+// Adds a click event listener to the close button which closes the overlay
+function setupClose() {
   const closeButton = document.querySelector('#close');
   if (closeButton != null) {
     closeButton.addEventListener('click', () => { document.location.href = '../index.html'; });
   }
-};
+}
